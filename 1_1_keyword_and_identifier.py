79302cf54f1a65c56b404b1c3f635973c214f5af
ywluohao <41532093+ywluohao@users.noreply.github.com>
2020-03-25 14:41:59 -0400

    Add files via upload

---
+++ 1_1_keyword_and_identifier.py
@@ -0,0 +1,38 @@
+#  1.1 Python Keyword and Identifier
+
+# Keyword
+# reserved
+# case sensitive
+# 33 keywords in Python 3.7
+# True, False, None
+print(True)
+print(False)
+print(true)
+print(TRUE)
+# others in low case
+# in, and, or, if, else, for, while ...
+
+# Identifier
+a_word = 'hello'
+print('hello')
+print(a_word)
+
+# 1. a-z, A-Z, 0-9, _
+# NO special characters, !@#$%^&
+# 2. csae sensitive
+print(A_word)
+print(a_word)
+# 3. can not start with digits
+variable1 = 123
+1variable = 1234
+_1variable = 12345
+# 4. keyword NO
+if = 123
+If = 12
+
+lastname = 'Smith'
+last_name = 'Smith'
+lastName = 'Smith'
+
+c = 100
+count = 100
